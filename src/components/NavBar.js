705c20ce5bda8f8d8ddcf87ff7eedfbe3f073c77
unknown <francoorellana1@gmail.com>
2020-08-20 13:18:41 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navlink } from "react-router-dom";
+import "./NavBar.css"
+
+export function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/productos" activeClassName="activeLink">
+                        Productos
+                    </NavLink>
+                    <hr />
+                </li>
+                <li>
+                    <NavLink to="/carrito" activeClassName="activeLink">
+                     Carrito
+                    </NavLink>
+                    <hr />
+                </li>               
+            </ul>
+        </nav>
+    );
+}
+
